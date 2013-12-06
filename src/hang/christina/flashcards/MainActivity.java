809d298a281fd7f1c34816c3962db0e5b92711b6
christinahang <christinah0113@gmail.com>
2013-12-05 23:41:14 -0800

    deck activities

--- src/hang/christina/flashcards/MainActivity.java
+++ src/hang/christina/flashcards/MainActivity.java
@@ -1,10 +1,14 @@
 package hang.christina.flashcards;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
 
 public class MainActivity extends Activity {
+	
+	public final String DECK_NAME = "hang.christina.MainActivity.DECK_NAME";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +21,20 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+	
+	public void createDeck(View v) {
+		Intent i = new Intent(this, ManageDeck.class);
+		// open window for user to input deck name
+		// save new deck to db and send name to ManageDeck activity
+		String deckName = "";
+		i.putExtra(DECK_NAME, deckName);
+		startActivity(i);
+	}
+	
+	public void viewDecks(View v) {
+		Intent i = new Intent(this, DeckList.class);
+		startActivity(i);
 	}
 
 }
